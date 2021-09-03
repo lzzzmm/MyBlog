1c438aeffb637f63b696639a24db3a856b82bcca
lzz <lzz8zmm@gmail.com>
2021-09-03 18:51:06 +0800

    fix:图片上传功能修复

--- myaccount/models.py
+++ myaccount/models.py
@@ -12,7 +12,7 @@ class UserProfile(models.Model):
     # 电话号码字段
     phone = models.CharField(max_length=20, blank=True)
     # 头像
-    avatar = models.ImageField(upload_to='avatar/%Y%m%d/', blank=True)
+    avatar = models.ImageField(upload_to='avatar/%Y%m%d/', default='default.png')
     # 个人简介
     bio = models.TextField(max_length=500, blank=True)
 
